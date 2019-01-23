8683b5709ee5a20b3b2fcce7c1ed8a56005609ab
jbaro <jbaro@brooktec.com>
2019-01-23 17:02:49 +0100

    add restrict country feature and clean code

--- bankFromIBAN.js
+++ bankFromIBAN.js
@@ -4,56 +4,49 @@
  * getBIC Return a Bic(SWIFT) code from Iban
  *
  * @param iban     The IBAN account.
+ * @param country  Limit result to one country
  * @return         The Bic(SWIFT) Code
  */
- function getBIC(iban) {
-     if (validateIBAN(iban)) {
-         var country = iban.substring(0, 2);
-         var banks = require("./AllCountries/" + country + ".json");
-         var bankCode = iban.substring(4, 8).replace(/^0+/, '');
+function getBIC(iban, country) {
+  let bank = getBankInfo(iban, country);
+  return bank ? bank.bic : "";
+}
 
-         var item = banks.list.find((d) => {
-             return d.id === bankCode
-         });
+/**
+ * getBankInfo Return full object from JSON data
+ *
+ * @param iban     The IBAN account.
+ * @param country  Limit result to one country
+ * @return         Bank information
+ */
+function getBankInfo(iban, country) {
+  if (validateIBAN(iban)) {
+    var ibanCountry = iban.substring(0, 2);
+    if (!country || country == ibanCountry) {
+      var banks = require("./AllCountries/" + ibanCountry + ".json");
+      var bankCode = parseInt(iban.substring(4, 8).replace(/^0+/, ''));
+      console.log(bankCode);
+      var item = banks.list.find((d) => {
+        return d.id === bankCode
+      });
+      if (item) {
+        item['bic'] = parse2BIC(item.swift_code);
+      }
+      return item;
+    }
+  }
+  return false;
+}
 
-         return item == undefined ? "" : item.swift_code.concat("XXXXXXXXXXX").substring(0,11);
-     } else {
-         return "";
-     }
- }
-
- /**
-  * getBankInfo Return full object from JSON data
-  *
-  * @param iban     The IBAN account.
-  * @return         Bank information
-  */
- function getBankInfo(iban) {
-     if (validateIBAN(iban)) {
-         var country = iban.substring(0, 2);
-         var banks = require("./AllCountries/" + country + ".json");
-         var bankCode = parseInt(iban.substring(4, 8).replace(/^0+/, ''));
-         var item = banks.list.find((d) => {
-             return d.id === bankCode
-         });
-         if(item) {
-           item['bic'] = parse2BIC(item.swift_code);
-         }
-         return item;
-     } else {
-         return false;
-     }
- }
-
- /**
-  * BIC formatter
-  *
-  * @param str     The swift code as a string.
-  * @return        The formatted string.
-  */
- function parse2BIC(str) {
-     return str.concat("XXXXXXXXXXX").substring(0,11);;
- }
+/**
+ * BIC formatter
+ *
+ * @param str     The swift code as a string.
+ * @return        The formatted string.
+ */
+function parse2BIC(str) {
+  return str.concat("XXXXXXXXXXX").substring(0, 11);;
+}
 
 /**
  * mod97 function for large numbers
@@ -62,11 +55,11 @@
  * @return        The number mod 97.
  */
 function _txtMod97(str) {
-    var res = 0;
-    for (var i = 0; i < str.length; i++) {
-        res = (res * 10 + parseInt(str[i], 10)) % 97;
-    }
-    return res;
+  var res = 0;
+  for (var i = 0; i < str.length; i++) {
+    res = (res * 10 + parseInt(str[i], 10)) % 97;
+  }
+  return res;
 }
 
 /**
@@ -76,8 +69,8 @@ function _txtMod97(str) {
  * @return            True, if the IBAN is valid.
  */
 function validateIBAN(iban) {
-    var ibrev = iban.substr(4) + iban.substr(0, 4);
-    return _txtMod97(_replaceChars(ibrev)) == 1;
+  var ibrev = iban.substr(4) + iban.substr(0, 4);
+  return _txtMod97(_replaceChars(ibrev)) == 1;
 }
 
 /**
@@ -88,24 +81,24 @@ function validateIBAN(iban) {
  * @return        The input string with letters replaced
  */
 function _replaceChars(str) {
-    var res = ""
-    for (var i = 0; i < str.length; i++) {
-        var cc = str.charCodeAt(i);
-        if (cc >= 65 && cc <= 90) {
-            res += (cc - 55).toString();
-        } else if (cc >= 97 && cc <= 122) {
-            res += (cc - 87).toString();
-        } else if (cc >= 48 && cc <= 57) {
-            res += str[i]
-        }
+  var res = ""
+  for (var i = 0; i < str.length; i++) {
+    var cc = str.charCodeAt(i);
+    if (cc >= 65 && cc <= 90) {
+      res += (cc - 55).toString();
+    } else if (cc >= 97 && cc <= 122) {
+      res += (cc - 87).toString();
+    } else if (cc >= 48 && cc <= 57) {
+      res += str[i]
     }
-    return res;
+  }
+  return res;
 }
 
 var exports = {
-    getBankInfo: getBankInfo,
-    getBIC: getBIC,
-    validateIBAN: validateIBAN
+  getBankInfo: getBankInfo,
+  getBIC: getBIC,
+  validateIBAN: validateIBAN
 }
 
 module.exports = exports;
